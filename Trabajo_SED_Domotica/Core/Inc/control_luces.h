b0589171fa6469dd86fa44101bfa56213fd553bd
raulherranzrodriguez <47772264+raulherranzrodriguez@users.noreply.github.com>
2022-01-18 00:02:25 +0100

    Revert "Revert "cambios varios""
    This reverts commit f0b43da6aa58b110e104a59e68824e273b36865c.

--- Trabajo_SED_Domotica/Core/Inc/control_luces.h
+++ Trabajo_SED_Domotica/Core/Inc/control_luces.h
@@ -10,15 +10,20 @@
 #include "main.h"
 
 ADC_HandleTypeDef hadc1;
-int estado_luces=0;
-int modo_luces=0; //0 manual 1 auto
+int estado_luces=0;//0 off 1 on 2 auto
 
 void setLuces(int n){
 	if(n==0){
 		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11, GPIO_PIN_RESET);
-
-	}else{
+			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	}else if(n==1){
 		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11, GPIO_PIN_SET);
+			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	}else if(n==2){
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
 	}
 	estado_luces=n;
 }
@@ -30,36 +35,16 @@ int getEstadoLuces(){
 void cambiarEstadoLuces(){
 		if(estado_luces==0){
 			setLuces(1);
-		}else{
+		}else if(estado_luces==1){
+			setLuces(2);
+		}else if(estado_luces==2){
 			setLuces(0);
 		}
 
 }
 
-void setModoLuces(int n){
-	if(n==0){
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-		}else{
-			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-		    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
-		}
-	modo_luces=n;
-}
-int getModoLuces(){
-	return modo_luces;
-}
-
-void cambiarModoLuces(){
-	if(modo_luces==0){
-		setModoLuces(1);
-	}else{
-		setModoLuces(0);
-	}
-}
-
 void medirLDR(){
-	if(modo_luces==1){
+	if(estado_luces==2){
 		int8_t adcval;
 			HAL_ADC_Start(&hadc1);
 			HAL_ADC_PollForConversion(&hadc1, 100);
