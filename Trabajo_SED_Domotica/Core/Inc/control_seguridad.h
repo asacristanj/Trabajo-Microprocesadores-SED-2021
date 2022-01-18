92cbaa9bab277afa61e3e2cab07e77128e724b18
raulherranzrodriguez <47772264+raulherranzrodriguez@users.noreply.github.com>
2022-01-18 18:43:51 +0100

    SEGURIDAD MEDIO FUNCIONANDO
    falta buzzer

--- Trabajo_SED_Domotica/Core/Inc/control_seguridad.h
+++ Trabajo_SED_Domotica/Core/Inc/control_seguridad.h
@@ -9,60 +9,82 @@
 #define INC_CONTROL_SEGURIDAD_H_
 #include "main.h"
 
-int estado_seguridad=0; //0 sistema apagado, 1 sistema encendiendose, 2 sistema_ activo
-int alarma_seguridad=0; //0 alarma apagada, 1 alarma encendida
+int estado_seguridad = 0; //0 sistema apagado, 1 sistema encendiendose, 2 sistema_ activo
+int alarma_seguridad = 0; //0 alarma apagada, 1 alarma encendida
 
-uint32_t tickstart_seguridad=0, counter_seguridad=0;
+uint32_t tickstart_seguridad = 0, counter_seguridad = 0;
 
-
-
-void setEstadoSeguridad(int n){
-	estado_seguridad=n;
-	if(estado_seguridad==0){
+void setEstadoSeguridad(int n) {
+	estado_seguridad = n;
+	if (estado_seguridad == 0) {
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
-		alarma_seguridad=0;
-	}else if(estado_seguridad==1){
-		alarma_seguridad=0;
-		tickstart_seguridad=HAL_GetTick();
-	}else if(estado_seguridad==2){
+		alarma_seguridad = 0;
+	} else if (estado_seguridad == 1) {
+		alarma_seguridad = 0;
+		tickstart_seguridad = HAL_GetTick();
+	} else if (estado_seguridad == 2) {
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
 	}
 }
 
-void activar_sensor(){
-	if(estado_seguridad==1&&counter_seguridad>5000){
-		counter_seguridad=0;
+void activar_sensor() {
+	/*
+	if (estado_seguridad == 1 && (counter_seguridad > 0)
+			&& (counter_seguridad < 1000)) {
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+	} else if (estado_seguridad == 1 && (counter_seguridad > 1000)
+			&& (counter_seguridad < 2000)) {
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+	} else if (estado_seguridad == 1 && (counter_seguridad > 2000)
+			&& (counter_seguridad < 3000)) {
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+	} else if (estado_seguridad == 1 && (counter_seguridad > 3000)
+			&& (counter_seguridad < 4000)) {
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+	} else if (estado_seguridad == 1 && counter_seguridad > 5000) {
+		counter_seguridad = 0;
+		setEstadoSeguridad(2);
+	}
+	*/
+	if (estado_seguridad == 1) {
+		counter_seguridad = HAL_GetTick() - tickstart_seguridad;
+		if ((counter_seguridad > 0) && (counter_seguridad < 500)) {
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+		}else if ((counter_seguridad > 500) && (counter_seguridad < 1000)) {
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+		} else if (counter_seguridad > 5000) {
+			counter_seguridad = 0;
 			setEstadoSeguridad(2);
-	}else if(estado_seguridad==1){
-		counter_seguridad=HAL_GetTick()-tickstart_seguridad;
+		}
+
 	}
 }
 
-int getEstadoSeguridad(){
+int getEstadoSeguridad() {
 	return estado_seguridad;
 }
 
-void cambiarEstadoSeguridad(){
-	if(getEstadoSeguridad()==0){
+void cambiarEstadoSeguridad() {
+	if (getEstadoSeguridad() == 0) {
 		setEstadoSeguridad(1);
-	}else {
+	} else {
 		setEstadoSeguridad(0);
 	}
 }
 
-void intruso_detectado(){
-	if(estado_seguridad==2){
-		alarma_seguridad=1;
+void intruso_detectado() {
+	if (estado_seguridad == 2) {
+		alarma_seguridad = 1;
 	}
 }
 
-void sonar_alarma(){
-if(estado_seguridad==2&&alarma_seguridad==1){
-	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11, GPIO_PIN_SET);
-}
+void sonar_alarma() {
+	if (estado_seguridad == 2 && alarma_seguridad == 1) {
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11, GPIO_PIN_SET);
+	}
 }
 
-void seguridad(){
+void seguridad() {
 	activar_sensor();
 	sonar_alarma();
 }
